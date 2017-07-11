c7c22b6288f2fe00ca76dfbdc8ef1e6b6dae09b7
Vertsinskaya Antanina <avertsinskaya@exadel.by>
2017-07-11 10:06:52 +0300

    Change login page, social buttons; add watch for index.sass

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('sass:lint', () => gulp.src(`${config.srcPath}/**/*.scss`)
 	}))
 );
 
-gulp.task('sass:watch', () => watch(`${config.srcPath}/**/*.scss`, () => {
+gulp.task('sass:watch', () => watch([`${config.srcPath}/**/*.scss`, `${config.srcPath}/index.sass`], () => {
 	gulp.start('sass:bundle');
 	gulp.start('sass:lint');
 }));
