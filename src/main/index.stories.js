54cae33cb3d6959bd3974c67481435dae7e6421d
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-03-10 13:48:45 +0800

    task(main): extract main page blocks [#63]

--- src/main/index.stories.js
+++ src/main/index.stories.js
@@ -1,13 +1,18 @@
 import { storiesOf } from '@storybook/html';
-import { select, color, boolean, text } from '@storybook/addon-knobs';
 import { withViewport } from '@storybook/addon-viewport';
 
-import renderFeatured from './featured.ejs';
+import renderFeatured from './blocks/featured.ejs';
+import renderDiary from './blocks/diary.ejs';
+import renderLatest from './blocks/latest.ejs';
+import renderTagsByTopic12 from './blocks/tags-by-topic-1-2.ejs';
+import renderTagsByTopic21 from './blocks/tags-by-topic-2-1.ejs';
+import renderTopic from './blocks/topic.ejs';
+import renderBrand from './blocks/brand.ejs';
+
+import './main.scss';
 
 import renderArticle from '../cards/article.ejs';
 import '../cards/article.scss';
-
-import './main.scss';
 
 import renderVideo from '../cards/video.ejs';
 import '../cards/video.scss';
@@ -24,51 +29,29 @@ import '../cards/person.scss';
 import { withIncludes } from '../../.storybook/utils';
 
 const render = withIncludes({
-  '../cards/article.ejs': renderArticle,
-  '../cards/video.ejs': renderVideo,
-  '../cards/collection.ejs': renderCollection,
-  '../cards/person.ejs': renderPerson,
-  '../cards/location.ejs': renderLocation,
+  '../../cards/article.ejs': renderArticle,
+  '../../cards/video.ejs': renderVideo,
+  '../../cards/collection.ejs': renderCollection,
+  '../../cards/person.ejs': renderPerson,
+  '../../cards/location.ejs': renderLocation,
 });
 
-const wrapper = content => `<div style="margin: 10px">${content}</div>`;
+const wrapper = content => `<div class="container">${content}</div>`;
 
-const cards = storiesOf('Main blocks', module).addDecorator(withViewport());
-cards.add('Featured', ({ parameters: { defaultData } }) => {
-  const size = select('size', ['xxl', 'xl', 'l', 'm', 'square-m', 'square-s'], 'xl');
-  const backgroundColor = color('backgroundColor', '#1a2e48');
-  const isDarkTheme = boolean('isDarkTheme', false);
-  const title = text('title', 'Як прыгатаваць мову ВКЛ?');
-  const author = text('author', 'Марыя Бадзей, Марына Анісімава');
-  const description = text(
-    'description',
-    'Мноства баталій, якія з зайздроснай рэгулярнасцю разгараюцца вакол беларускай мовы, тычацца не толькі яе сучаснага стану. Мовазнаўцы і гісторыкі і па сёння не могуць пагадзіцца наконт таго, на якую мову Скарына пераклаў Біблію, што можна лічыць моваю беларускай і з якіх часоў варта адсочваць яе развіццё. Паспрабуем прыгледзецца да мовы Вялікага Княства Літоўскага'
-  );
+const blocks = storiesOf('Main Blocks', module).addDecorator(withViewport());
 
-  const data = {
-    ...defaultData,
-    size,
-    backgroundColor,
-    isDarkTheme,
-    title,
-    author,
-    description,
-  };
-
-  return wrapper(render(renderFeatured, data));
-});
-
-cards.add('Main', ({ parameters: { defaultData } }) => {
-  // const size = select('size', ['xxl', 'xl', 'l', 'm', 'square-m', 'square-s'], 'xl');
-  const backgroundColor = color('backgroundColor', '#1a2e48');
-  // const isDarkTheme = boolean('isDarkTheme', false);
-
-  const data = {
-    ...defaultData,
-    // size,
-    backgroundColor,
-    // isDarkTheme,
-  };
-
-  return wrapper(render(renderFeatured, data));
-});
+blocks.add('Featured', ({ parameters: { defaultData } }) =>
+  wrapper(render(renderFeatured, defaultData))
+);
+blocks.add('Diary', ({ parameters: { defaultData } }) => wrapper(render(renderDiary, defaultData)));
+blocks.add('Latest', ({ parameters: { defaultData } }) =>
+  wrapper(render(renderLatest, defaultData))
+);
+blocks.add('TagsByTopic12', ({ parameters: { defaultData } }) =>
+  wrapper(render(renderTagsByTopic12, defaultData))
+);
+blocks.add('TagsByTopic21', ({ parameters: { defaultData } }) =>
+  wrapper(render(renderTagsByTopic21, defaultData))
+);
+blocks.add('Topic', ({ parameters: { defaultData } }) => wrapper(render(renderTopic, defaultData)));
+blocks.add('Brand', ({ parameters: { defaultData } }) => wrapper(render(renderBrand, defaultData)));
